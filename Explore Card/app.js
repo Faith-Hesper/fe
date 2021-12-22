983a43fc49c686cd6aa21666efe5ff6c20e307f4
Faith-Hesper <2310298446@qq.com>
2021-12-22 20:34:50 +0800

    add

--- Explore Card/app.js
+++ Explore Card/app.js
@@ -22,3 +22,5 @@ function upload(obj) {
         img.setAttribute("src",this.result)
     }
 }
+
+
--- WeChat/js/random.js
+++ WeChat/js/random.js
@@ -1,6 +1,4 @@
-
 let names = ["李鑫", '刘黎', '潘石屹', '吴海安', '李雅静', ' 韩梦洁', '王安晏', '赵博超', '钟哲圣', '李沁', '张雨琪', '张雨绮', '李世民', '邓海洋', '梦琪', '王忆柳', '张惜文', '李慕青', '叶安志']
 let usr_name = names[Math.floor(Math.random() * names.length)];
 $Cookie("姓名", usr_name, {});
 $Cookie("姓名");
-// $Cookie("姓名",null);
