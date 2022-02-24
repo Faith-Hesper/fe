1519e4818e4c11dfa4e6b1a1f9e6c21f4252d599
Faith-Hesper <2310298446@qq.com>
2022-02-24 12:54:58 +0800

    更新

--- vue-demo/src/api/base.js
+++ vue-demo/src/api/base.js
@@ -1,10 +1,13 @@
 import axios from 'axios'
+// `baseURL` 将自动加在 `url` 前面，除非 `url` 是一个绝对 URL。
+// 它可以通过设置一个 `baseURL` 便于为 axios 实例的方法传递相对 URL
 axios.defaults.baseURL = '/interface.sina.cn'
 
 const base = {
-    host: 'http://api.tianapi.com/',
+    host: 'https://api.tianapi.com/',
     covInfo: 'ncov/index?key=61abdb275a0249299eeb6392a8f53e07',
-    provinceData: '/news/wap/fymap2020_data.d.json'
+    prohost: 'https://interface.sina.cn/',
+    provinceData: '/wap/fymap2020_data.d.json'
 }
 
 const headers = {
@@ -23,7 +26,8 @@ const api = {
         return axios({
             url: base.provinceData,
             method: 'get',
-            headers: headers
+            headers: headers,
+            responseType: 'json'
         })
     }
 }
--- vue-demo/vue.config.js
+++ vue-demo/vue.config.js
@@ -5,17 +5,34 @@
 const path = require('path')
 
 module.exports = {
+    publicPath: './',//基本路径
+    outputDir: "dist",
+    assetsDir: 'static',//放置生成的静态资源 (js、css、img、fonts) 的目录
+    indexPath: "index.html",
+    productionSourceMap: false,//生产环境是否生成 sourceMap 文件
     devServer: {
         proxy: {
             // 在这里配置如下代码
-            '/interface.sina.cn': {
-                target: 'https://interface.sina.cn', // 你请求的第三方接口
+            '/interface.sina.cn/': {
+                target: 'https://interface.sina.cn/news', // 将/interface.sina.cn代理为https://interface.sina.cn/
+                ws: true,
                 changeOrigin: true, // 在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
+                secure: true,
                 pathRewrite: {
                     // 路径重写，
-                    '^/interface.sina.cn': '', // 替换target中的请求地址，也就是说以后你在请求http://api.qingyunke.com/api.php这个地址的时候直接写成/api即可。
+                    '^/interface.sina.cn/': '', // 替换target中的请求地址，也就是说以后你在请求http://api.qingyunke.com/api.php这个地址的时候直接写成/api即可。
                 },
             },
+            '/api': {
+                target: 'https://api.tianapi.com', //API服务器的地址
+                ws: true, //代理websockets
+                changeOrigin: true, // 是否跨域，虚拟的站点需要更管origin
+                secure: true,
+                pathRewrite: {
+                    // '^/api'是一个正则表达式，表示要匹配请求的url中，全部'http://localhost:8081/api' 转接为 http://localhost:8081/
+                    '^/api.tianapi.com': '',
+                }
+            }
         },
     }
 }
