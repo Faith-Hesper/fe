c752f81c178c016c0973fecb7b2d5b2aacdb0eda
Faith-Hesper <2310298446@qq.com>
2022-02-28 18:32:46 +0800

    rm

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -25,36 +25,7 @@ export default {
         }
     },
     methods: {
-        getData() {
-            document.cookie = 'traceid=d814dd0725;'
-            document.cookie =
-                'SID=033b4332fdbd406a857843c85eb4408476e3cc87360f4457a93667012d548202;'
-            document.cookie = 'TOK=d814dd07258b9a0d;'
-            document.cookie = 'hashkey=d814dd07;'
-            document.cookie = 'optimal_cdn_domain=docs2.gtimg.com;'
-            document.cookie = 'backup_cdn_domain=docs2.gtimg.com;'
-            document.cookie = 'adtag=s_wx_mini_docs_1044'
-            // const data = await axios.get('http://api.tianapi.com/topnews/index?key=61abdb275a0249299eeb6392a8f53e07')
-            axios({
-                url: `/qq/form/collect/submit?u=378577316257478d82daff4cffbaad8d`,
-                method: 'post',
-                data: {
-                    global_padid: '300000000$JvMpfxzeUqtR',
-                    data: '[{"id":"SIMPLE1599549650232ketmk94o","type":"SIMPLE","content":"陈海滨","dataType":1,"securityStatus":false},{"id":"RADIO1645514705566kzxsywte","type":"RADIO","content":["正常，36.3-37.2℃"],"dataType":1,"selectedOptionIds":["RADIOOPTkzxsywtc"]},{"id":"RADIO1645514840444kzxt1svy","type":"RADIO","content":["无"],"dataType":1,"selectedOptionIds":["RADIOOPTkzxt1svx"]},{"id":"RADIO1645514874513kzxt2j6b","type":"RADIO","content":["居家"],"dataType":1,"selectedOptionIds":["RADIOOPTkzxt2j69"]},{"id":"RADIO1645515041538kzxt641x","type":"RADIO","content":["否"],"dataType":1,"selectedOptionIds":["RADIOOPTkzxt641w"]},{"id":"RADIO1645515070444kzxt6qcu","type":"RADIO","content":["无"],"dataType":1,"selectedOptionIds":["RADIOOPTkzxt6qct"]},{"id":"LOCATION1645515086581kzxt72t1","type":"LOCATION","content":"江西省/赣州市/瑞金市/城东朗朗幼儿园东南(红都大道东)","dataType":1}]',
-                    role: {
-                        hideIds: [],
-                        sid: 26,
-                    },
-                    modify_sid: 26,
-                },
-            }).then((response) => {
-                    console.log(response)
-                }).catch((error) => {
-                    console.log(error)
-                })
-            // this.text = res.newslist
-            // console.log(res)
-        },
+        
     },
     created() {
         
