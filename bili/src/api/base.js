2387f59523342969c83c9dcabc9c8db71c909622
Faith-Hesper <2310298446@qq.com>
2022-03-06 15:41:50 +0800

    async

--- bili/src/api/base.js
+++ bili/src/api/base.js
@@ -1,5 +1,26 @@
 import axios from 'axios'
 axios.defaults.withCredentials = true
+
+// bvid->aid->cid
+async function bili_download(bvid) {
+    const aid = await axios.get(`/bili/x/web-interface/archive/stat?bvid=${bvid}`).then(res => {
+        const { data: { data: { aid } } } = res
+        return aid
+    }).catch(err => console.log(err))
+    
+    const cid = await axios.get(`/bili/x/web-interface/view?aid=${aid}`).then(res => {
+        const { data: { data: { cid } } } = res
+        return cid
+    }).catch(err => console.log(err))
+    
+    const durl = await axios.get(`/bili/x/player/playurl?avid=${aid}&cid=${cid}&qn=112`).then(res => {
+        const { data: { data: { durl } } } = res
+        return durl
+    }).catch(err => console.log(err))
+    
+    return durl
+}
+
 const api = {
     up_personal_inform(mid) {
         return axios.get(`/bili/x/space/acc/info?mid=${mid}&jsonp=jsonp`)
@@ -13,16 +34,6 @@ const api = {
     getvideoList(mid,ps=30,pn=1) {
         return axios.get(`/bili/x/space/arc/search?mid=${mid}&ps=${ps}&tid=0&pn=${pn}&keyword=&order=pubdate&jsonp=jsonp`)
     },
-    // bvid->aid->cid
-    get_aid(bvid) {
-        return axios.get(`/bili/x/web-interface/archive/stat?bvid=${bvid}`)
-    },
-    get_cid(aid) {
-        return axios.get(`/bili/x/web-interface/view?aid=${aid}`)
-    },
-    bili_downloadUrl(aid, cid) {
-        return axios.get(`/bili/x/player/playurl?avid=${aid}&cid=${cid}&qn=112`)
-    }
 }
-
+export { bili_download }
 export default api
